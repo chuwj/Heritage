0aedea3c965c8abc4f1fff04d9a1ae223aa082b3
chuwenjun <chuwj2011@hotmail.com>
2025-02-19 08:04:47 +0800

    修改后初次提交

---
+++ heritage-api/heritage-api-system/src/main/java/com/heritage/system/api/RemoteFileService.java
@@ -0,0 +1,29 @@
+package com.heritage.system.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import com.heritage.common.core.constant.ServiceNameConstants;
+import com.heritage.common.core.domain.R;
+import com.heritage.system.api.domain.SysFile;
+import com.heritage.system.api.factory.RemoteFileFallbackFactory;
+
+/**
+ * 文件服务
+ * 
+ * @author ruoyi
+ */
+@FeignClient(contextId = "remoteFileService", value = ServiceNameConstants.FILE_SERVICE, fallbackFactory = RemoteFileFallbackFactory.class)
+public interface RemoteFileService
+{
+    /**
+     * 上传文件
+     *
+     * @param file 文件信息
+     * @return 结果
+     */
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public R<SysFile> upload(@RequestPart(value = "file") MultipartFile file);
+}
